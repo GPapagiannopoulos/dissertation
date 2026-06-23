083382cee685b11f3df7bcd6a8ec00d2a640e49c
George <georgiosmyronpapagiannopoulos@gmail.com>
2026-06-23 11:13:52 +0100

    feat: pyhealth orientation

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,7 +4,18 @@ from pathlib import Path
 from dotenv import load_dotenv
 from pyhealth.datasets import MIMIC4Dataset
 
-load_dotenv()
-DATA_PATH = Path(os.getenv("DATA_DIRECTORY"))
-mimic4_dataset = MIMIC4Dataset(DATA_PATH)
-print(mimic4_dataset.dataset_name)
+if __name__ == "__main__":
+    load_dotenv()
+    EHR_ROOT = Path(os.getenv("MIMIC_IV_EHR_DATA"))
+    mimic4_dataset = MIMIC4Dataset(
+        ehr_root=EHR_ROOT,
+        dev=True,
+        ehr_tables=["patients", "admissions", "diagnoses_icd"],
+    )
+
+    mimic4_dataset.load_data()
+
+    patient_id = mimic4_dataset.unique_patient_ids[0]
+    patient = mimic4_dataset.get_patient(patient_id)
+    patient = patient.get_events(event_type="admissions", return_df=True)
+    print(patient[[s.name for s in patient if not (s.null_count() == patient.height)]])
