00d19878647d86112de0ff544f8df24b666d46f7
George <georgiosmyronpapagiannopoulos@gmail.com>
2026-07-06 09:29:35 +0100

    feat: implement tests for happy path col conversions

---
+++ tests/data/test_sources.py
@@ -0,0 +1,62 @@
+"""Test suite for the EDASource class methods and helper functions."""
+
+import polars as pl
+import pytest
+
+from thesis.data.sources import cast_frame
+
+
+@pytest.mark.parametrize(
+    "data, schema_overrides, expected_dtype",
+    [
+        (
+            {"col": pl.Series(["a", "b", "c"], dtype=pl.String)},
+            {"col": "String"},
+            pl.String,
+        ),
+        (
+            {"col": pl.Series(["1", "2", "3"], dtype=pl.String)},
+            {"col": "UInt"},
+            pl.UInt16,
+        ),
+        (
+            {"col": pl.Series(["1.1", "2.2", "3.3"], dtype=pl.String)},
+            {"col": "Float"},
+            pl.Float32,
+        ),
+        (
+            {"col": pl.Series(["1", "0", "0"], dtype=pl.String)},
+            {"col": "Boolean"},
+            pl.Boolean,
+        ),
+        (
+            {
+                "col": pl.Series(
+                    ["2025-01-01", "2026-01-01", "2027-01-01"], dtype=pl.String
+                )
+            },
+            {"col": "Date"},
+            pl.Date,
+        ),
+        (
+            {
+                "col": pl.Series(
+                    [
+                        "2025-01-01 12:15:00",
+                        "2026-01-01 12:15:00",
+                        "2027-01-01 12:15:00",
+                    ],
+                    dtype=pl.String,
+                )
+            },
+            {"col": "DateTime"},
+            pl.Datetime,
+        ),
+    ],
+)
+def test_happy_path_conversions(
+    data: dict[str, pl.Series], schema_overrides: dict[str, str], expected_dtype: type
+) -> None:
+    """Asserts that simple dtype castings work as intended."""
+    lf = cast_frame(pl.LazyFrame(data), schema_overrides)
+    assert lf.collect_schema()["col"] == expected_dtype
