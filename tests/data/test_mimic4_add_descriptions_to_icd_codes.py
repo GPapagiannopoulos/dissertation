13e7dd9039ff7366ea4bdb9ac9f351ed30f5beb7
George <georgiosmyronpapagiannopoulos@gmail.com>
2026-07-11 14:00:51 +0100

    feat: add error handling test, make raised KeyError symmetric

--- tests/data/test_mimic4_add_descriptions_to_icd_codes.py
+++ tests/data/test_mimic4_add_descriptions_to_icd_codes.py
@@ -112,3 +112,87 @@ def test_add_descriptions_to_icd_codes_happy_path(
     joined_lf = mimic4_add_descriptions_to_icd_codes(source, path, event_type)
     expected_lf = pl.LazyFrame(expected_lf_data)
     assert_frame_equal(joined_lf, expected_lf)
+
+
+@pytest.mark.parametrize(
+    "map_data, lf_data, event_type",
+    [
+        # 0. Mapping df is missing icd_code field
+        (
+            {
+                "icd_version": pl.Series(["a"], dtype=pl.String),
+                "icd_name": pl.Series(["123"], dtype=pl.String),
+                "long_title": pl.Series(["description"], dtype=pl.String),
+            },
+            {
+                "icd_version": pl.Series(["a"], dtype=pl.String),
+                "icd_code": pl.Series(["123"], dtype=pl.String),
+            },
+            "event_type",
+        ),
+        # 1. Mapping df is missing icd_version field
+        (
+            {
+                "icd_revision": pl.Series(["a"], dtype=pl.String),
+                "icd_code": pl.Series(["123"], dtype=pl.String),
+                "long_title": pl.Series(["description"], dtype=pl.String),
+            },
+            {
+                "icd_version": pl.Series(["a"], dtype=pl.String),
+                "icd_code": pl.Series(["123"], dtype=pl.String),
+            },
+            "event_type",
+        ),
+        # 2. LazyFrame is missing icd_code
+        (
+            {
+                "icd_version": pl.Series(["a"], dtype=pl.String),
+                "icd_code": pl.Series(["123"], dtype=pl.String),
+                "long_title": pl.Series(["description"], dtype=pl.String),
+            },
+            {
+                "icd_version": pl.Series(["a"], dtype=pl.String),
+                "icd_name": pl.Series(["123"], dtype=pl.String),
+            },
+            "event_type",
+        ),
+        # 3. LazyFrame is missing icd_version
+        (
+            {
+                "icd_version": pl.Series(["a"], dtype=pl.String),
+                "icd_code": pl.Series(["123"], dtype=pl.String),
+                "long_title": pl.Series(["description"], dtype=pl.String),
+            },
+            {
+                "icd_revision": pl.Series(["a"], dtype=pl.String),
+                "icd_code": pl.Series(["123"], dtype=pl.String),
+            },
+            "event_type",
+        ),
+        # 4. LazyFrame has an icd_version for the wrong event_type
+        (
+            {
+                "icd_version": pl.Series(["a"], dtype=pl.String),
+                "icd_code": pl.Series(["123"], dtype=pl.String),
+                "long_title": pl.Series(["description"], dtype=pl.String),
+            },
+            {
+                "other_type/icd_version": pl.Series(["a"], dtype=pl.String),
+                "icd_name": pl.Series(["123"], dtype=pl.String),
+            },
+            "event_type",
+        ),
+    ],
+)
+def test_add_descriptions_to_icd_raises(
+    mapping_csv: Callable,
+    source_lazyframe: Callable,
+    map_data: dict[str, pl.Series],
+    lf_data: dict[str, pl.Series],
+    event_type: str,
+) -> None:
+    """Asserts that missing columns raise an error."""
+    path = mapping_csv(map_data, event_type)
+    source = source_lazyframe(event_type, **lf_data)
+    with pytest.raises(KeyError):
+        mimic4_add_descriptions_to_icd_codes(source, path, event_type)
