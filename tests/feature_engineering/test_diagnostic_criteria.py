213d4e058860b00908a738236a6a930bebb346f4
George <georgiosmyronpapagiannopoulos@gmail.com>
2026-07-15 11:39:19 +0100

    feat: scaffolding for diagnostic criteria and tests

---
+++ tests/feature_engineering/test_diagnostic_criteria.py
@@ -0,0 +1,9 @@
+"""Test suite for the helper functions under diagnostic_criteria."""
+
+"""
+Desired behaviour includes:
+1) identifies a patient with increase in serum creatinine by
+26micromol/ 0.3mg within 48 hours
+2) increase in serum creatinine to >1.5x baseline in the last 7 days
+3) Urine volume <0.5mL/kg/hour - labevents/description == Urine Volume, Total
+"""
