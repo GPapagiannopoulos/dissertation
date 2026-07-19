dca705e3307c0c1cfdda9d18eb3d3500bb64e435
George <georgiosmyronpapagiannopoulos@gmail.com>
2026-07-19 15:28:41 +0100

    feat: output is sorted by subject_id and subsequently hadm_id

--- tests/feature_engineering/test_normalize_weights.py
+++ tests/feature_engineering/test_normalize_weights.py
@@ -41,6 +41,21 @@ from thesis.feature_engineering.urine_output import normalize_weights
                 "valuenum": [90.627756] * 6,
             },
         ),
+        # 2. Output is subsequently sorted by hadm_id
+        (
+            {
+                "subject_id": ["2"] * 3 + ["1"] * 3,
+                "hadm_id": ["3", "2", "1"] * 2,
+                "charttiime": [datetime.datetime(2025, 1, 1, 0)] * 6,
+            },
+            {
+                "subject_id": ["1"] * 3 + ["2"] * 3,
+                "hadm_id": ["1", "2", "3"] * 2,
+                "stay_id": ["1"] * 6,
+                "charttiime": [datetime.datetime(2025, 1, 1, 0)] * 6,
+                "valuenum": [90.627756] * 6,
+            },
+        ),
     ],
 )
 def test_normalize_weights_happy_path(
