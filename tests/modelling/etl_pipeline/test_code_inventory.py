9824992388c150daf433779f0fad6b65b28a5185
George <georgiosmyronpapagiannopoulos@gmail.com>
2026-07-27 17:34:27 +0100

    refactor: test_code_inventory_happy_path now works with parametrize

--- tests/modelling/etl_pipeline/test_code_inventory.py
+++ tests/modelling/etl_pipeline/test_code_inventory.py
@@ -1,20 +1,34 @@
 """Testing suite for the code_inventory helper."""
 
+from collections.abc import Callable
+
 import polars as pl
+import pytest
 from polars.testing import assert_frame_equal
 
 from thesis.modelling.etl_pipeline.coverage import code_inventory
 
 
-def test_code_inventory_groups_by_code() -> None:
+@pytest.mark.parametrize(
+    "drops, overrides, expected",
+    [
+        # 0. Standard happy path
+        (
+            [],
+            {},
+            {
+                "code": pl.Series(["a", "b", "c"], dtype=pl.String),
+                "count": pl.Series([1] * 3, dtype=pl.UInt32),
+            },
+        )
+    ],
+)
+def test_code_inventory_happy_path(
+    meds_outputs: Callable,
+    drops: list[str],
+    overrides: dict[str, pl.Series],
+    expected: dict[str, pl.Series],
+) -> None:
     """Asserts standard intended behaviour for code_inventory."""
-    data = {
-        "subject_id": pl.Series(["1", "2", "3"], dtype=pl.String),
-        "code": pl.Series(["a", "b", "c"], dtype=pl.String),
-    }
-    expected = {
-        "code": pl.Series(["a", "b", "c"], dtype=pl.String),
-        "count": pl.Series([1] * 3, dtype=pl.UInt32),
-    }
-    data_lf = pl.LazyFrame(data)
+    data_lf = pl.LazyFrame(meds_outputs(drops, **overrides))
     assert_frame_equal(code_inventory(data_lf), pl.LazyFrame(expected))
