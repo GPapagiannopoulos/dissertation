427c8c918ab1831439dacf36636380328964371e
George <georgiosmyronpapagiannopoulos@gmail.com>
2026-07-30 16:12:41 +0100

    feat: extract dtype casting and renaming of labels to script

--- src/thesis/modelling/cohort/pos_diagnoses.py
+++ src/thesis/modelling/cohort/pos_diagnoses.py
@@ -1,18 +1,15 @@
 """Module for extracting patients with a positive AKI diagnosis as per criteria."""
 
-from thesis.feature_engineering.driver import driver
+import polars as pl
 
 
-def identify_surviving_aki_admissions():
+def identify_surviving_aki_admissions(
+    labels: pl.LazyFrame, events: pl.LazyFrame
+) -> pl.LazyFrame:
     """Identifies the admissions with valid tokens post normalization.
 
     The process of normalizing MIMIC-IV data resulted into dropping of
     events that could not be mapped to any valid MOTOR token. For positive
     diagnosis events to be useful they need surviving events.
     """
-    diagnoses = driver()
-    print(diagnoses.collect_schema().names())
-
-
-if __name__ == "__main__":
-    identify_surviving_aki_admissions()
+    return pl.LazyFrame()
