6a3756afee6d5cb93e57b7b48e8fab9179d475e4
George <georgiosmyronpapagiannopoulos@gmail.com>
2026-07-31 12:52:34 +0100

    feat: implement core logic for splitting into folds

--- src/thesis/modelling/cohort/split.py
+++ src/thesis/modelling/cohort/split.py
@@ -1,5 +1,10 @@
 """Module for handling the splitting of the cohort into training/validation/testing."""
 
+import math
+from hashlib import sha256
+from itertools import accumulate
+from struct import pack
+
 import polars as pl
 
 _POSITIVE_LABEL = "positive"
@@ -110,5 +115,60 @@ def build_subject_strata(
                 separator=_STRATUM_SEPARATOR,
             )
         )
+        .sort("subject_id")
+    )
+
+
+def assign_folds(
+    strata: pl.DataFrame, fractions: dict[str, float], seed: int
+) -> pl.DataFrame:
+    """Assigns individuals of each stratum to a cohort.
+
+    The function uses hashing to assign subjects to their respective
+    cohorts.Hshing is used because unlike random.shuffle it isn't
+    dependent on the original order, it is computable per subject,
+    and it offers decorrelation from the id structure. We are not using
+    the native polars pl.col().hash() method because it is not guaranteed
+    stable across releases.
+
+    Splitting into cohorts is done at the subject level because MOTOR
+    featurizes an admission from the patient's entire prior timeline.
+    Including a patient in numerous cohorts is data leakage.
+    """
+    if not math.isclose(1.0, sum(fractions.values())):
+        raise ValueError(
+            f"Fractions sum to {sum(fractions.values())}.Must sum to 1.0 instead."
+        )
+
+    breaks = list(accumulate(fractions.values()))[:-1]
+    names = list(fractions.keys())
+
+    subject_ids = strata["subject_id"]
+    # noinspection PyTypeChecker
+    hashed_subject_ids = pl.Series(
+        [
+            int(sha256(pack(">q", seed) + pack(">q", x)).hexdigest()[:16], 16)
+            for x in subject_ids
+        ],
+        dtype=pl.UInt64,
+    )
+    strata_hashed_ids = (
+        strata.with_columns(hashed_subject_ids=hashed_subject_ids)
+        .sort(["hashed_subject_ids", "subject_id"])
+        .with_columns(
+            (
+                pl.int_range(pl.len()).over(pl.col("stratum"))
+                / pl.len().over("stratum")
+            ).alias("rank")
+        )
+    )
+
+    return (
+        strata_hashed_ids.with_columns(
+            pl.col("rank")
+            .cut(breaks=breaks, labels=names, left_closed=True)
+            .alias("fold")
+        )
+        .select("subject_id", "stratum", "fold")
         .sort("subject_id")
     )
